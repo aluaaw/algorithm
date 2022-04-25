4d2d00495068591ffd76dcd21fc0619edbc81751
aluaaw <ylua.aw@gmail.com>
2022-04-25 09:13:42 +0900

    소스정리

--- src/main/java/com/toy/algorithm/queue/QueueService.java
+++ src/main/java/com/toy/algorithm/queue/QueueService.java
@@ -9,16 +9,6 @@ import java.util.Arrays;
 @RequiredArgsConstructor
 public class QueueService {
     // TODO: qSize를 정해두고 해당 값을 infinity로 사용할 수 있도록 하라.
-    // TODO: qSize = 10;
-    // TODO: 여기에서 구현해야 할 건 원형큐인데 원형큐의 사이즈와 값이 들어왔을 때의 선형큐 위치, 값이 나갔을 때의 선형큐 위치이다.
-    // TODO: 값이 들어오면 input은 p +1의 값을 가진다. 여기서 p는 position이다. 보통 position은 index값으로 계산하며 index의 처음값은 0이다.
-    // TODO: 값이 나가면 output은 P +1의 값을 가진다. 여기서 p는 똑같이 position이다.
-    // TODO: input과 output은 처음 index값에서 시작하므로 0이 된다.
-    // TODO: input과 output이 동일하게 되면 공백이거나, 포화상태이거나. size와 input이나 output이 동일했을 때 같은 건 선형큐일 때만 해당되는 이야기. 왜냐면 input을 다 하지 않을 상태에서 output을 조금만 하고 그다음에 input이 가득 찼을 때 = size값과 동일할 때 포화상태는 아니지 않은가.
-    // TODO: 그러므로 여기에서 구현되어야 할 건,
-    // TODO: 원래 배열에서 새로운 값이 추가되었을 때의 처리 (포화상태인지 아닌지를 확인하는 예외처리)
-    // TODO: 원래 배열에서 기존 값을 출력하였을 때의 처리 (공백상태인지 아닌지를 확인하는 예외처리)
-    // TODO: 초기값은 0으로 통일한다.
     private final int size = 10;
     private int inputPosition = 0;
     private int outputPosition = 0;
